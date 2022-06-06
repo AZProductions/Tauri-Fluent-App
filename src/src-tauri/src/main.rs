61187649b35d7c4101305c570d83ce15da5395e0
AZProductions <66299945+AZProductions@users.noreply.github.com>
2022-06-06 15:24:57 +0200

    chore: updated packages and added window shadow

--- src/src-tauri/src/main.rs
+++ src/src-tauri/src/main.rs
@@ -4,7 +4,18 @@
 )]
 
 fn main() {
+  // tauri::Builder::default()
+  //   .run(tauri::generate_context!())
+  //   .expect("error while running tauri application");
   tauri::Builder::default()
+    .setup(|app| {
+        use tauri::Manager;
+        use window_shadows::set_shadow;
+
+        let window = app.get_window("main").unwrap();
+        let _ = set_shadow(&window, true); // Don't use unwrap() here as it will panic on Linux.
+        Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/src/routes/__layout.svelte
+++ src/src/routes/__layout.svelte
@@ -2,4 +2,28 @@
 	import 'fluent-svelte/theme.css';
 </script>
 
+<div class="titlebar" />
+<div class="spacer" />
 <slot />
+
+<style>
+	:global(body) {
+		background-color: var(--fds-solid-background-base);
+		color: var(--fds-text-primary);
+	}
+
+	.titlebar {
+		display: block;
+		position: fixed;
+		height: 48px;
+		top: 0;
+		left: 0;
+		width: calc(100% - 2px);
+		z-index: 999;
+		background: #333333;
+	}
+	.spacer {
+		width: 100%;
+		height: 48px;
+	}
+</style>
